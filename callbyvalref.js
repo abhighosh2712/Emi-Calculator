5f15ed1aeb79a6006ae31c7c8f8436eb308f2132
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2026-04-04 15:32:47 +0530

    add

---
+++ callbyvalref.js
@@ -0,0 +1,20 @@
+
+var a=10;
+var b={name:'sachin'}
+
+function callByValRef(x,y){
+  x=20
+  y.name='ramesh'
+
+}
+// befor calling the function
+console.log("a",a)
+console.log("b",b)
+
+callByValRef(a,b)
+console.log("a",a)
+console.log("b",b)
+
+
+
+
---
+++ restoperator.js
@@ -0,0 +1,6 @@
+function userInfo(...myaddress) {
+     console.log("myaddress", myaddress)
+}
+userInfo("Alice", 30, "New York");
+
+
