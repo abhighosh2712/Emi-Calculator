5f54d688fecacc787656b2d487d130715f9ff403
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2026-04-11 15:35:50 +0530

    add

--- ObjectOrented.js
+++ ObjectOrented.js
@@ -1,27 +1,24 @@
-class Student {
 
-constructor(_name, _age, _grade) {
-  this.name = _name;
-  this.age = _age;
-  this.grade = _grade;
-  console.log("Student created");
-  console.log("Name:", this.name);
-  console.log("Age:", this.age);
-  console.log("Grade:", this.grade);
+class Father{
+
+  constructor(name){
+    this.name=name
+  }
+  getProperty(){
+    console.log("name",this.name)
+  }
+  getALlDetails() {
+    console.log("name", this.name);
+  }
 }
 
-    getDetails() {
-      return this.name + " is " + this.age + " years old and has grade " + this.grade;
-    }
-    setName(newName) {
-      this.name = newName;
-    }
-    getName() {
-      return this.name;
-    }
 
+class Child extends Father{
+  constructor(name,age){
+    super(name)
+    this.age=age
+  }
 }
-let s1=new Student('Abhi', 20, 'A');
-s1.setName('Ramesh');
-var name=s1.getName();
-console.log("Name:", name);
+
+let child1=new Child("sachin",30)
+child1.getProperty()
